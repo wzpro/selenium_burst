1e1f76fb0b253f64073aa9ad7accc40966ffb083
Reset <110873701+wzpro@users.noreply.github.com>
2025-08-26 17:27:50 +0800

    Update config.py

--- selenium_burst/config.py
+++ selenium_burst/config.py
@@ -19,7 +19,9 @@ DRIVER_PATH = "D:\Desktop\/newx\\reset_py\pc\chromedriver-win64\chromedriver.exe
 # 超时设置
 TIMEOUT = 5                 # 页面元素等待（秒）
 REQUEST_TIMEOUT = 2         # 获取验证码HTTP超时（秒）
-
+PAGE_LOAD_STRATEGY = "normal"  # 正常加载策略
 
 # 性能优化
 HEADLESS = False            # 关闭无头，显示浏览器窗口
+# 每个密码最大尝试次数（建议1，防止重复输入）
+MAX_ATTEMPTS_PER_PASSWORD = 1
